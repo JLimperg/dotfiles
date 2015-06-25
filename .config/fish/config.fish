2bf8b39c702f2fa22b9e836a2582c892e570ad09
Jannis Limperg <jannis@limperg.de>
2015-06-25 03:01:52 +0200

    Removes nix
    Switching Haskell development to `stack`, which integrates much better
    with the existing system (by design). This makes nix obsolete.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set -e fish_greeting
 
-set -gx PATH ~/bin $PATH
+set -gx PATH ~/bin /opt/bin $PATH
 set -gx EDITOR vim
 set -gx VISUAL vim
 set -gx PAGER less
@@ -21,24 +21,4 @@ alias vgaoff "xrandr --output VGA1 --off"
 
 function dox
   xinit $argv > ~/.xinit.log 2>&1 &
-end
-
-# Nix
-
-set -gx SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt
-set -gx NIX_LINK "$HOME/.nix-profile"
-set -gx PATH     "$NIX_LINK/bin" "$NIX_LINK/sbin" $PATH
-set -gx NIX_PATH "$NIX_PATH:nixpkgs=$HOME/.nix-defexpr/nixpkgs"
-set -gx NIX_USER_PROFILE_DIR "$PROFILE_PREFIX/per-user/$USER"
-set -gx NIX_PROFILES /var/lib/nix/profiles/default "$HOME/.nix-profile"
-set -gx NIX_USER_GCROOTS_DIR /var/lib/nix/gcroots/per-user/$USER
-
-if test $USER -eq root
-  set -e NIX_REMOTE
-else
-  set -gx NIX_REMOTE daemon
-end
-
-for p in $NIX_PROFILES
-  set -gx PATH $p/bin $PATH
 end
