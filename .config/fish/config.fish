f30a54b763bfca1edbc27a5c4cb7d018ce533472
Jannis Limperg <jannis@limperg.de>
2015-07-01 21:18:31 +0200

    fish: better env var handling
    a) The PATH is now prepended to only by login shells. This prevents a
    minor problem where the PATH would grow with each nested fish
    invocation.
    
    b) Most former global variables are now universal since this seems to be
    the idiomatic fish way of doing things. Not sure yet if this is actually
    an improvement.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,7 @@
-set -e fish_greeting
-
-set -gx PATH ~/bin /opt/bin $PATH
-set -gx EDITOR vim
-set -gx VISUAL vim
-set -gx PAGER less
-set -gx MANPAGER 'less -isR'
+if status --is-login
+  set -e fish_greeting
+  set -x PATH ~/bin ~/.local/bin $PATH
+end
 
 # Aliases
 
@@ -13,7 +10,7 @@ alias cal 'cal -m'
 alias pdflatex 'pdflatex -interaction nonstopmode'
 alias xclip 'xclip -selection c'
 alias mailx 'mkdir -p /tmp/mailx; and env TMPDIR=/tmp/mailx mailx'
-alias suspend 'sudo /usr/sbin/pm-suspend'
+alias sus 'sudo /usr/sbin/pm-suspend'
 
 alias tournaman="wine ~/'.wine/drive_c/Program Files (x86)/Tournaman/Tournaman.exe'"
 alias vgaon "xrandr --output VGA1 --auto --primary --output LVDS1 --off; and xrandr --output LVDS1 --auto"
