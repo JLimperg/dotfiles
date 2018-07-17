9e86718978cd635ee151c1d0da7880773e83957a
Jannis Limperg <jannis@limperg.de>
2018-07-17 16:58:28 +0200

    Add autojump support to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,8 @@ alias cal 'cal -m'
 alias pdflatex 'pdflatex -interaction nonstopmode'
 alias xclip 'xclip -selection c'
 
+# Autojump
 
+if test -f /usr/share/autojump/autojump.fish
+  source /usr/share/autojump/autojump.fish
+end
