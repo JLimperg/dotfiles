82903ce5f2be78cc81a2968f0184afc5a3698ad8
Jannis Limperg <jannis@limperg.de>
2023-03-29 12:37:12 +0200

    fish: add alias for emacsclient

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -5,6 +5,8 @@ set fish_greeting
 alias cal 'cal -m'
 alias pdflatex 'pdflatex -interaction nonstopmode'
 alias xclip 'xclip -selection c'
+alias emacsc 'emacsclient'
+
 alias fsk 'cd ~/uni/fsk-timi-2023'
 alias aec 'cd ~/uni/serve/git-icfp-aec/events/2023'
 alias aesop 'cd ~/uni/aesop'
