1842609d2488d9cc7ff8b026de27aaadc4091780
Jannis Limperg <jannis@limperg.de>
2023-05-02 15:47:59 +0200

    fish: add exit misspelling alias

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -6,6 +6,7 @@ alias cal 'cal -m'
 alias pdflatex 'pdflatex -interaction nonstopmode'
 alias xclip 'xclip -selection c'
 alias emacsc 'emacsclient --no-wait'
+alias exti 'exit'
 
 alias fsk 'cd ~/uni/fsk-timi-2023'
 alias aec 'cd ~/uni/serve/git-icfp-aec/events/2023'
